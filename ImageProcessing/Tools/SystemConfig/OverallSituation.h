c3cfdfca0a0bd1d6aebe7db6120f189ad62e62a2
zhangxiaosong <zhangxiaosong@jsj.com.cn>
2018-11-05 17:54:10 +0800

    .

---
+++ ImageProcessing/Tools/SystemConfig/OverallSituation.h
@@ -0,0 +1,26 @@
+//
+//  OverallSituation.h
+//  ImageProcessing
+//
+//  Created by Zhang xiaosong on 2018/11/5.
+//  Copyright © 2018 Zhang xiaosong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+extern NSString * const ZXSFont;
+
+extern NSString * const 
+
+
+
+@interface OverallSituation : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ImageProcessing/Tools/SystemConfig/OverallSituation.m
@@ -0,0 +1,20 @@
+//
+//  OverallSituation.m
+//  ImageProcessing
+//
+//  Created by Zhang xiaosong on 2018/11/5.
+//  Copyright © 2018 Zhang xiaosong. All rights reserved.
+//
+
+#import "OverallSituation.h"
+
+
+
+NSString * const ZXSFont = @"PingFangSC-Regular";
+
+
+@implementation OverallSituation
+
+
+
+@end
