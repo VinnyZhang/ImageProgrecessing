c13964e6ec29d4df2a11709b4199b607cf1ee83a
zhangxiaosong <zhangxiaosong@jsj.com.cn>
2018-11-05 17:22:58 +0800

    first

---
+++ ImageProcessing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageProcessing
+//
+//  Created by Zhang xiaosong on 2018/11/5.
+//  Copyright © 2018 Zhang xiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageProcessing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageProcessing
+//
+//  Created by Zhang xiaosong on 2018/11/5.
+//  Copyright © 2018 Zhang xiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
